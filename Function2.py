74eb0900f82837e8d5eb6d63509c2f5fbebd1b08
papasmf1 <papasmf1@gmail.com>
2021-12-14 10:44:55 +0900

    함수를 연습2

--- Function2.py
+++ Function2.py
@@ -63,3 +63,19 @@ def union(*ar):
 print( union("HAM","SPAM") )
 print( union("HAM","SPAM","EGG") )
 
+#정의되지 않은 인자 처리 
+def userURIBuilder(server, port, **user):
+    strURL = "http://" + server + ":" + port + "/?"
+    for key in user.keys():
+        strURL += key + "=" + user[key] + "&"
+    return strURL 
+
+#호출 
+print( userURIBuilder("credu.com","80",id="kim",name="mike") )
+print( userURIBuilder("credu.com","80",id="kim",name="mike",   
+    age="30") )
+
+#람다함수(이름이 없는 간결한 함수 정의)
+g = lambda x,y:x*y
+print( g(3,4) )
+print( g(5,6) )
