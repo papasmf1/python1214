edf3c8fca5f564ba6e4106a6f9b656d582d1c897
papasmf1 <papasmf1@gmail.com>
2021-12-14 14:17:47 +0900

    Person클래스 연습

--- class1.py
+++ class1.py
@@ -9,6 +9,23 @@ class Person:
 
 #인스턴스 생성
 p1 = Person()
+p1.name = "전우치"
+p2 = Person() 
+
 p1.print()
+p2.print() 
+
+#런타임(실행시간에 추가)
+Person.title = "new title"
+print(p1.title)
+print(p2.title)
+print(Person.title)
+
+#인스턴스에 추가
+p1.age = 30
+print(p1.age)
+#print(p2.age)
+
+
 
 
