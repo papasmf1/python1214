5057cf1841e6503905ded0ea59b738fb36530635
papasmf1 <papasmf1@gmail.com>
2021-12-16 14:38:28 +0900

    웹 크롤링 완료

--- 오늘의유머.py
+++ 오늘의유머.py
@@ -8,13 +8,12 @@ import re
 #우리 사이트는 웹봇금지 
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
-# <span class="subject_fixed" data-role="list-title-text" title="레오폴드 NP900RBT PD 그레이 블루 (갈축)">
-# 		레오폴드 NP900RBT PD 그레이 블루 (갈축)
-# </span>
 
-for n in range(0,10):
-        #클리앙의 중고장터 주소 
-        data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
+#파일에 저장
+f = open("c:\\work\\today.txt", "wt", encoding="utf-8")
+for n in range(1,11):
+        #오늘의 유머
+        data ='http://www.todayhumor.co.kr/board/list.php?table=bestofbest&page=' + str(n)
         print(data)
         #웹브라우져 헤더 추가 
         req = urllib.request.Request(data, \
@@ -23,15 +22,25 @@ for n in range(0,10):
         #혹시 한글이 깨지는 경우? 
         page = data.decode('utf-8', 'ignore')
         soup = BeautifulSoup(page, 'html.parser')
+
+        #<td class=subject>
+        # <a href="/board/view.php?table=bestofbest&amp;no=448797&amp;s_no=448797&amp;page=1" 
+        # target="_top">과거 등교거부 사유</a>
+
         #attrs: 어트리뷰트들 
-        list = soup.find_all('span', attrs={'data-role':'list-title-text'})
+        list = soup.find_all('td', attrs={'class':'subject'})
 
         for item in list:
                 try:
-                        title = item.text.strip()
-                        if (re.search('애플', title)):
-                                print(title)
+                        item2 = item.find("a").text.strip()  
+                        #print(item2)
+                        if (re.search('한국', item2)):
+                                print(item2)
+                                f.write(item2 + "\n")
 
                 except:
                         pass
         
+
+f.close() 
+
