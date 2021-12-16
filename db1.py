a09f785e338bc27c1f980b69e13db6034075a5db
papasmf1 <papasmf1@gmail.com>
2021-12-16 09:39:21 +0900

    데이터베이스연습3

--- db1.py
+++ db1.py
@@ -14,13 +14,27 @@ cur.execute("create table PhoneBook (Name text, PhoneNum text);")
 
 #1건 입력
 cur.execute("insert into PhoneBook values ('derick', '010-222');")
+#입력 파라메터 처리(?, ?) ==>입력을 받는다.  
+name = "gildong"
+phoneNumber = "010-123"
+cur.execute("insert into PhoneBook values (?,?);", (name,phoneNumber))
+#다중의 행 데이터 입력(2차원 행열데이터, 2건의 레코드)
+datalist = (("tom","010-111"),("dsp","010-456"))
+cur.executemany("insert into PhoneBook values (?,?);", datalist)
 
 #검색
 cur.execute("select * from PhoneBook;")
-for row in cur:
-    #print( row[0] + " , " + row[1] )
-    print(row)
+#다중라인 주석: ctrl + / 
+# for row in cur:
+#     #print( row[0] + " , " + row[1] )
+#     print(row)
 
+print("---fetchone()---")
+print( cur.fetchone() )
+print("---fetchmany(2)---")
+print( cur.fetchmath(2) )
+print("---fetchall()---")
+print( cur.fetchall() )
 
 
 
