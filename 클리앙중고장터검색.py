7045a5ec139f7e6f030f214ba49d07d35a43d365
papasmf1 <papasmf1@gmail.com>
2021-12-16 14:14:06 +0900

    클리앙 중고 장터 검색

--- 클리앙중고장터검색.py
+++ 클리앙중고장터검색.py
@@ -1,31 +1,38 @@
 # coding:utf-8
 from bs4 import BeautifulSoup
 import urllib.request
+#정규표현식 
 import re 
 
 #User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
+#우리 사이트는 웹봇금지 
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
+
+# <span class="subject_fixed" data-role="list-title-text" title="레오폴드 NP900RBT PD 그레이 블루 (갈축)">
+# 		레오폴드 NP900RBT PD 그레이 블루 (갈축)
+# </span>
 
 for n in range(0,10):
         #클리앙의 중고장터 주소 
         data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
+        print(data)
         #웹브라우져 헤더 추가 
         req = urllib.request.Request(data, \
                                     headers = hdr)
         data = urllib.request.urlopen(req).read()
+        #혹시 한글이 깨지는 경우? 
         page = data.decode('utf-8', 'ignore')
         soup = BeautifulSoup(page, 'html.parser')
-        list = soup.findAll('a', attrs={'class':'list_subject'})
+        #attrs: 어트리뷰트들 
+        list = soup.find_all('span', attrs={'data-role':'list-title-text'})
 
         for item in list:
                 try:
-                        #<a class='list_subject'><span>text</span><span>text</span>
-                        span = item.contents[1]
-                        span2 = span.nextSibling.nextSibling
-                        title = span2.text 
-                        if (re.search('아이폰', title)):
-                                print(title.strip())
-                                print('https://www.clien.net'  + item['href'])
+                        title = item.text.strip()
+                        print(title) 
+                        # if (re.search('아이폰', title)):
+                        #         print(title.strip())
+
                 except:
                         pass
         
