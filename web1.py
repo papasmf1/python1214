aff706f65acc39e636264186023177f9f63d6b7b
papasmf1 <papasmf1@gmail.com>
2021-12-16 11:38:28 +0900

    크롤링 연습3

--- web1.py
+++ web1.py
@@ -2,12 +2,34 @@
 #웹 크롤링을 위한 선언 
 from bs4 import BeautifulSoup
 
-#연습용 파일을 로딩
+#연습용 파일을 로딩(Read Text)
+#메서드 체인:메서드를 연속으로 호출
 page = open("c:\\work\\test01.html", "rt", encoding="utf-8").read()
 
-#검색이 용이한 객체 
+#검색이 용이한 객체(HTML태그에서 검색) 
 soup = BeautifulSoup(page, "html.parser")
 
 #전체 내용 보기 
-print( soup.prettify() )
+#print( soup.prettify() )
+
+#<p> 전부 검색 ==> List에 담아서 리턴 
+#print( soup.find_all("p") )
+
+#<p>한개
+#print( soup.find("p") )
+
+#검색조건: <p class="outer-text">
+#파이썬의 예약어(키워드)는 변수명, 인자이름을 사용하면 안됨
+#수많은 <p>에서 필터링(걸러내기) 
+#print( soup.find_all("p", class_ = "outer-text") )
+
+#<p id="first"> 검색
+#print( soup.find_all("p", id="first") )
+
+#내부에 컨텐츠만 가져오기
+for item in soup.find_all("p"):
+    content = item.text
+    print(content)
+
+
 
